f48824eb1eecff8bb88d25d57e5c105581925b39
Tolgonai Andakulova <tolgonaia@gmail.com>
2023-02-27 20:32:55 +0600

    started clientInfo section

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {TextField} from "@mui/material";
 
 
-const InputForm = ({id, label, type}) => {
+const InputForm = ({id, label, type, margin, fullWidth, inputWidth}) => {
   return (
     <TextField
       id={id}
       label={label}
       variant='standard'
       type={type}
+      fullWidth={fullWidth}
       sx={{
-        padding: '5px 30px',
+        padding: '5px 30px 7px',
         background: '#F6F6FC',
+        borderRadius: '5px',
+        marginRight: margin,
+        maxWidth: inputWidth,
+        marginBottom: '30px'
       }}
       InputProps={{
         disableUnderline: true,
@@ -19,6 +24,7 @@ const InputForm = ({id, label, type}) => {
       InputLabelProps={{
         variant: 'filled',
         shrink: 'true',
+        style: {padding: '0 20px', color:'rgba(0,0,0,0.5)'}
       }}
     />
   );
--- src/containers/OrderPlace/OrderPlace.js
+++ src/containers/OrderPlace/OrderPlace.js
@@ -7,12 +7,50 @@ const OrderPlace = () => {
   return (
     <Box className='order-place'>
       <Typography sx={{padding: '0', fontSize: '20px'}} >Оформление заказа</Typography>
-      <Box>
-        <InputForm
-          id="name"
-          label="Имя"
-          type="password"
-        />
+      <Box sx={{marginTop: '30px', display: 'flex', justifyContent: 'space-between'}}>
+        <div className='order-place-block' style={{width: '48%'}}>
+          <InputForm
+            id="name"
+            label="Имя"
+            type="text"
+            fullWidth='true'
+          />
+          <div style={{display: 'flex'}}>
+            <InputForm
+              id="phone"
+              label="Телефон"
+              type="tel"
+              margin='15px'
+              fullWidth='true'
+            />
+            <InputForm
+              id="add"
+              label="Доб"
+              type="number"
+              inputWidth='70px'
+            />
+          </div>
+          <InputForm
+            id="addPhone"
+            label="Доп телефон или whatsapp"
+            type="tel"
+            fullWidth='true'
+          />
+        </div>
+        <div className='order-place-block' style={{width: '48%'}}>
+          <InputForm
+            id="lastName"
+            label="Фамилия"
+            type="text"
+            fullWidth='true'
+          />
+          <InputForm
+            id="email"
+            label="E-mail"
+            type="mail"
+            fullWidth='true'
+          />
+        </div>
       </Box>
     </Box>
   );
