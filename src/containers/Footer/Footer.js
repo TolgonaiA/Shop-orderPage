1a54e1371b32dbbc2209ea247ca3a9097a96d364
Tolgonai Andakulova <tolgonaia@gmail.com>
2023-03-03 11:14:42 +0600

    footer

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Box, Container, Link, Typography} from "@mui/material";
 import './Footer.css';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 
 const data = {
@@ -8,20 +11,54 @@ const data = {
   'Категории': ['Бумага', 'Письмо и графика', 'Канц товары', 'Художественные товары', 'Творчество', 'Полиграфические материалы', 'Распродажи', 'Офисное оборудование', 'Игры и игрушки', 'Сувенирная продукция'],
 }
 
+const contacts = {
+  'Огонбаева, 222': ['0777 90 22 33', '0312 90 22 33'],
+  'Эркиндик, 7': ['0777 90 22 43', '0312 90 22 34'],
+  'Уметалиева, 84': ['0777 90 22 33', '0312 90 22 33']
+}
+
 const Footer = () => {
   return (
     <Box sx={{background: '#262626'}}>
       <Container className='footer-wrap' maxWidth='lg'>
         <Typography className='footer-title'>Kanzler&Bürger</Typography>
-        <Box sx={{display:'flex'}}>
+        <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
           {Object.keys(data).map(el =>
             <Box sx={{display: 'flex', flexDirection: 'column'}}>
-              <Typography sx={{color: '#fff'}}>{el}</Typography>
+              <Typography className='footer-subtitle'>{el}</Typography>
               {data[el].map(item => (
-                <Link>{item}</Link>
+                <Link className='footer-item'>{item}</Link>
               ))}
             </Box>
           )}
+          <Box>
+            <Typography className='footer-subtitle'>Контакты</Typography>
+            {Object.keys(contacts).map(el =>
+              <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                <Typography className='footer-item'>{el}</Typography>
+                {contacts[el].map(item => (
+                  <Typography className='footer-item'>{item}</Typography>
+                ))}
+              </Box>
+            )}
+          </Box>
+          <Box>
+            <Typography className='footer-subtitle'>Мы в соцсетях</Typography>
+            <Box sx={{display: 'flex', flexDirection: 'column'}}>
+              <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <InstagramIcon className='footer-item'/>
+                <Link className='footer-item'>Instagram</Link>
+              </Box>
+              <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <FacebookIcon className='footer-item'/>
+                <Link className='footer-item'>Facebook</Link>
+              </Box>
+              <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <WhatsAppIcon className='footer-item'/>
+                <Link className='footer-item'>What's App</Link>
+              </Box>
+            </Box>
+          </Box>
         </Box>
       </Container>
     </Box>
